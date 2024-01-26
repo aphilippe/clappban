155c64c60ddfe76ef9e2afdaaf86b28f49e440f4
Adrien Philippe <adrien.philippe@ubisoft.com>
2024-01-26 15:42:02 +0100

    Display column
    No real visual

--- Avalonia/Clappban/Clappban/ViewModels/BoardViewModel.cs
+++ Avalonia/Clappban/Clappban/ViewModels/BoardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Clappban.Models.Boards;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Clappban.Models.Boards;
 
 namespace Clappban.ViewModels;
 
@@ -6,8 +8,13 @@ public class BoardViewModel : ViewModelBase
 {
     private readonly IBoardRepository _boardRepository;
 
+    public Board Board => _boardRepository.CurrentBoard!;
+    public IEnumerable<ColumnViewModel> Columns { get; }
+    
     public BoardViewModel(IBoardRepository boardRepository)
     {
         _boardRepository = boardRepository;
+
+        Columns = _boardRepository.CurrentBoard.Columns.Select(x => new ColumnViewModel(x)).ToList();
     }
 }
---
+++ Avalonia/Clappban/Clappban/ViewModels/ColumnViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Clappban.Models.Boards;
+
+namespace Clappban.ViewModels;
+
+public class ColumnViewModel : ViewModelBase
+{
+    private readonly Column _column;
+
+    private string _title;
+    public string Title => _column.Title;
+
+    public ColumnViewModel(Column column)
+    {
+        _column = column;
+    }
+}
